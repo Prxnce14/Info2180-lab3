a74f93d41591f6d59f9c7da91fd99cd5a735b545
Prxnce14 <100068441+Prxnce14@users.noreply.github.com>
2022-10-15 18:47:20 -0500

    added click events to squares

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,20 @@
-
-
 window.onload = function()
-{
-    
+{   
+    //This gets all the sqaure elemts and stores them in a variable.
+    const square_elements = document.querySelectorAll(".square")
+
+    //looping through each fo the sqaure elements
+    square_elements.forEach(square =>
+    {   
+        //every time we click the square it should perform the handleclick function once.  so it should not be repeated
+        square.addEventListener('click', handleclick, {once: true})
+    })
+
+    //this function  takes parameter event
+    function handleclick(e)
+    {
+        console.log('clicked')
+    }
 }
+
+
